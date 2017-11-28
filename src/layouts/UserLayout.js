96f2a54c67bceaaf5dfd0e7bfcd84cef553480ba
apple <apple@appledeMacBook-Pro.local>
2017-11-28 23:05:11 +0800

    login

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, Route } from 'dva/router';
+import { Route } from 'dva/router';
 import DocumentTitle from 'react-document-title';
-import { Icon } from 'antd';
-import GlobalFooter from '../components/GlobalFooter';
 import styles from './UserLayout.less';
-
-const links = [{
-  title: '帮助',
-  href: '',
-}, {
-  title: '隐私',
-  href: '',
-}, {
-  title: '条款',
-  href: '',
-}];
-
-const copyright = <div>Copyright <Icon type="copyright" /> 2017 蚂蚁金服体验技术部出品</div>;
 
 class UserLayout extends React.PureComponent {
   static childContextTypes = {
@@ -44,15 +29,6 @@ class UserLayout extends React.PureComponent {
     return (
       <DocumentTitle title={this.getPageTitle()}>
         <div className={styles.container}>
-          <div className={styles.top}>
-            <div className={styles.header}>
-              <Link to="/">
-                <img alt="" className={styles.logo} src="https://gw.alipayobjects.com/zos/rmsportal/NGCCBOENpgTXpBWUIPnI.svg" />
-                <span className={styles.title}>Ant Design</span>
-              </Link>
-            </div>
-            <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
-          </div>
           {
             getRouteData('UserLayout').map(item =>
               (
@@ -65,7 +41,6 @@ class UserLayout extends React.PureComponent {
               )
             )
           }
-          <GlobalFooter className={styles.footer} links={links} copyright={copyright} />
         </div>
       </DocumentTitle>
     );
