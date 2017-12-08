5e537aca7f36579b5a2f55093209beee297ecda6
sosout <sosout@139.com>
2017-12-08 17:48:43 +0800

    主页布局

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -228,69 +228,70 @@ class BasicLayout extends React.PureComponent {
     const layout = (
       <Layout className={styles['basic-layout']}>
         <Header className={styles['basic-header']}>
+        头部
         </Header>
-      <Layout style={{ marginLeft: 200 }}>
+        <Layout style={{ marginLeft: 200 }}>
           <Sider style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}>
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-            <Menu.Item key="1">
-              <Icon type="user" />
-              <span className="nav-text">nav 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span className="nav-text">nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
-              <span className="nav-text">nav 3</span>
-            </Menu.Item>
-            <Menu.Item key="4">
-              <Icon type="bar-chart" />
-              <span className="nav-text">nav 4</span>
-            </Menu.Item>
-            <Menu.Item key="5">
-              <Icon type="cloud-o" />
-              <span className="nav-text">nav 5</span>
-            </Menu.Item>
-            <Menu.Item key="6">
-              <Icon type="appstore-o" />
-              <span className="nav-text">nav 6</span>
-            </Menu.Item>
-            <Menu.Item key="7">
-              <Icon type="team" />
-              <span className="nav-text">nav 7</span>
-            </Menu.Item>
-            <Menu.Item key="8">
-              <Icon type="shop" />
-              <span className="nav-text">nav 8</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-          <div style={{ minHeight: 'calc(100vh - 260px)' }}>
-            <Switch>
-              {
-                getRouteData('BasicLayout').map(item =>
-                  (
-                    <Route
-                      exact={item.exact}
-                      key={item.path}
-                      path={item.path}
-                      component={item.component}
-                    />
+            <div className="logo" />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+              <Menu.Item key="1">
+                <Icon type="user" />
+                <span className="nav-text">nav 1</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="video-camera" />
+                <span className="nav-text">nav 2</span>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="upload" />
+                <span className="nav-text">nav 3</span>
+              </Menu.Item>
+              <Menu.Item key="4">
+                <Icon type="bar-chart" />
+                <span className="nav-text">nav 4</span>
+              </Menu.Item>
+              <Menu.Item key="5">
+                <Icon type="cloud-o" />
+                <span className="nav-text">nav 5</span>
+              </Menu.Item>
+              <Menu.Item key="6">
+                <Icon type="appstore-o" />
+                <span className="nav-text">nav 6</span>
+              </Menu.Item>
+              <Menu.Item key="7">
+                <Icon type="team" />
+                <span className="nav-text">nav 7</span>
+              </Menu.Item>
+              <Menu.Item key="8">
+                <Icon type="shop" />
+                <span className="nav-text">nav 8</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+            <div style={{ minHeight: 'calc(100vh - 260px)' }}>
+              <Switch>
+                {
+                  getRouteData('BasicLayout').map(item =>
+                    (
+                      <Route
+                        exact={item.exact}
+                        key={item.path}
+                        path={item.path}
+                        component={item.component}
+                      />
+                    )
                   )
-                )
-              }
-              <Redirect exact from="/" to="/dashboard/analysis" />
-            </Switch>
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©2016 Created by Ant UED
-        </Footer>
+                }
+                <Redirect exact from="/" to="/dashboard/analysis" />
+              </Switch>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2016 Created by Ant UED
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
     );
     return (
       <DocumentTitle title={this.getPageTitle()}>
