0b99fc301fbf17b7d049bbeff063bda3c675baf1
apple <apple@appledeMacBook-Pro.local>
2017-12-11 23:23:15 +0800

    菜单调整

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -138,69 +138,69 @@ class BasicLayout extends React.PureComponent {
           >
             {this.getNavMenuItems(item.children, itemPath)}
             <Menu.Item key="1">
-          <Icon type="user" />
-          <span className="nav-text">nav 1</span>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Icon type="video-camera" />
-          <span className="nav-text">nav 2</span>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Icon type="upload" />
-          <span className="nav-text">nav 3</span>
-        </Menu.Item>
-        <Menu.Item key="4">
-          <Icon type="bar-chart" />
-          <span className="nav-text">nav 4</span>
-        </Menu.Item>
-        <Menu.Item key="5">
-          <Icon type="cloud-o" />
-          <span className="nav-text">nav 5</span>
-        </Menu.Item>
-        <Menu.Item key="6">
-          <Icon type="appstore-o" />
-          <span className="nav-text">nav 6</span>
-        </Menu.Item>
-        <Menu.Item key="7">
-          <Icon type="team" />
-          <span className="nav-text">nav 7</span>
-        </Menu.Item>
-        <Menu.Item key="8">
-          <Icon type="shop" />
-          <span className="nav-text">nav 8</span>
-        </Menu.Item>
-        <Menu.Item key="11">
-          <Icon type="user" />
-          <span className="nav-text">nav 1</span>
-        </Menu.Item>
-        <Menu.Item key="21">
-          <Icon type="video-camera" />
-          <span className="nav-text">nav 2</span>
-        </Menu.Item>
-        <Menu.Item key="31">
-          <Icon type="upload" />
-          <span className="nav-text">nav 3</span>
-        </Menu.Item>
-        <Menu.Item key="41">
-          <Icon type="bar-chart" />
-          <span className="nav-text">nav 4</span>
-        </Menu.Item>
-        <Menu.Item key="51">
-          <Icon type="cloud-o" />
-          <span className="nav-text">nav 5</span>
-        </Menu.Item>
-        <Menu.Item key="61">
-          <Icon type="appstore-o" />
-          <span className="nav-text">nav 6</span>
-        </Menu.Item>
-        <Menu.Item key="71">
-          <Icon type="team" />
-          <span className="nav-text">nav 7</span>
-        </Menu.Item>
-        <Menu.Item key="81">
-          <Icon type="shop" />
-          <span className="nav-text">nav 8</span>
-        </Menu.Item>
+              <Icon type="user" />
+              <span className="nav-text">nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span className="nav-text">nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span className="nav-text">nav 3</span>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Icon type="bar-chart" />
+              <span className="nav-text">nav 4</span>
+            </Menu.Item>
+            <Menu.Item key="5">
+              <Icon type="cloud-o" />
+              <span className="nav-text">nav 5</span>
+            </Menu.Item>
+            <Menu.Item key="6">
+              <Icon type="appstore-o" />
+              <span className="nav-text">nav 6</span>
+            </Menu.Item>
+            <Menu.Item key="7">
+              <Icon type="team" />
+              <span className="nav-text">nav 7</span>
+            </Menu.Item>
+            <Menu.Item key="8">
+              <Icon type="shop" />
+              <span className="nav-text">nav 8</span>
+            </Menu.Item>
+            <Menu.Item key="11">
+              <Icon type="user" />
+              <span className="nav-text">nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="21">
+              <Icon type="video-camera" />
+              <span className="nav-text">nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="31">
+              <Icon type="upload" />
+              <span className="nav-text">nav 3</span>
+            </Menu.Item>
+            <Menu.Item key="41">
+              <Icon type="bar-chart" />
+              <span className="nav-text">nav 4</span>
+            </Menu.Item>
+            <Menu.Item key="51">
+              <Icon type="cloud-o" />
+              <span className="nav-text">nav 5</span>
+            </Menu.Item>
+            <Menu.Item key="61">
+              <Icon type="appstore-o" />
+              <span className="nav-text">nav 6</span>
+            </Menu.Item>
+            <Menu.Item key="71">
+              <Icon type="team" />
+              <span className="nav-text">nav 7</span>
+            </Menu.Item>
+            <Menu.Item key="81">
+              <Icon type="shop" />
+              <span className="nav-text">nav 8</span>
+            </Menu.Item>
           </SubMenu>
         );
       }
